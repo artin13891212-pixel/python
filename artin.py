28f4184c08ac5292f99212a498a2ddab747391e1
artin hajirahim <artin13891212@gmail.com>
2025-09-05 01:06:25 -0700

    python1

---
+++ artin.py
@@ -0,0 +1,38 @@
+numebr = 3
+
+pi = 3.14
+
+name = "ahmad"
+
+text = 'i love "python" so much'
+
+bool
+
+print(numebr)
+
+print(type(3))
+
+print(type(5.0))
+
+a=5
+b=10
+c=15
+print(a+b+c)
+
+name="artin"
+print(name)
+
+print(bool)
+
+
+print(sum(flag))
+
+
+
+number1=1
+number2=9
+print(sum(flag))
+
+_sum(number1+number2)
+print(sum)
+
